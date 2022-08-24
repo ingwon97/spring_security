ff608ca42b903be8a525f3977aee06be449fdc99
ingwon97 <ghkdrnjs180@naver.com>
2022-08-25 04:01:05 +0900

    2) 필터 초기화와 다중 보안 설정

--- basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -2,6 +2,7 @@ package io.security.basicsecurity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,15 +10,30 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
+@Order(0)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .antMatcher("/admin/**")
                 .authorizeRequests()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+        .and()
+                .httpBasic();
+    }
+}
+
+@Configuration
+@EnableWebSecurity
+@Order(1)
+class SecurityConfig2 extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
                 .formLogin();
-        http
-                .csrf();
     }
 }
